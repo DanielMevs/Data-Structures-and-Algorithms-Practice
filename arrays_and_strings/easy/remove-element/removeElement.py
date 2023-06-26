9a6063557a9306f1e199957e4b4843261f777723
DanielMevs <dannymevs@hotmail.com>
2023-06-26 19:22:58 -0400

    contiinued work on remove element

--- arrays_and_strings/easy/remove-element/removeElement.py
+++ arrays_and_strings/easy/remove-element/removeElement.py
@@ -5,3 +5,13 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        count = 0
+        i = 0
+        for num in nums:
+            if num == val:
+                nums.pop(i)
+                i += 1
+                count += 1
+            else:
+                i +=1
+        return count
