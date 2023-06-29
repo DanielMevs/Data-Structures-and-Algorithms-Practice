e4eb7337a72d7cb7f06250f8988fd52ca3bcc389
DanielMevs <dannymevs@hotmail.com>
2023-06-28 23:14:45 -0400

    debugged attempt 1 of remove duplicates of sorted list

---
+++ arrays_and_strings/easy/remove_dups/debug.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_nums = []
+        for num in nums:
+            if num not in unique_nums:
+                unique_nums.append(num)
+                print(f'num: {num}')
+                print(f'unique nums: {unique_nums}')
+            else:
+                num_idx = nums.index(num)
+                nums.pop(num_idx)
+        k = len(unique_nums)
+        print(f'Nums at the end: {nums}')
+        return k
+    
+nums = [1,1,2]
+
+remDup = Solution()
+remDup.removeDuplicates(nums=nums)
