0e370eae10a0f89b4e4108f0db68a2515f0acb07
DanielMevs <dannymevs@hotmail.com>
2023-07-06 23:24:05 -0400

    completed find length of last word in a string

---
+++ arrays_and_strings/easy/length_of_last_word/solution2.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # A tracker to find what a non-white-space sequence is found
+        is_found = False
+        i = len(s) - 1
+        count = 0
+        while i >= 0:
+            if s[i] ==' ' and is_found == False:
+                i -= 1
+            elif is_found == False and s[i] != ' ':
+                count += 1
+                i -= 1
+                is_found = True
+            elif is_found == True and s[i] != ' ':
+                count += 1
+                i -= 1
+                
+            else:
+                break
+            
+            
+        return count
