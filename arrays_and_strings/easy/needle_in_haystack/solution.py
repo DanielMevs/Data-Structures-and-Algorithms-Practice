60d4ee3d008d32314758905ba2533152985bd742
DanielMevs <dannymevs@hotmail.com>
2023-07-08 20:08:33 -0400

    completed find index of first occurring needle in string haystack  exercise

---
+++ arrays_and_strings/easy/needle_in_haystack/solution.py
@@ -0,0 +1,10 @@
+# Find the Index of the First Occurrence in a String
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
