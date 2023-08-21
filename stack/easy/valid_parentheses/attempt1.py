1c59ca3ca487a789e2b4cd0c71ed00d30c7a0f41
DanielMevs <dannymevs@hotmail.com>
2023-08-21 18:42:16 -0400

    completed valid parentheses

--- stack/easy/valid_parentheses/attempt1.py
+++ stack/easy/valid_parentheses/attempt1.py
@@ -1,4 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        charStack = []
         closing_dict = {'}': '{', ')': '(', ']':'['}
-        return
+        
+        for char in s:
+            if char in closing_dict:
+                if charStack and charStack[-1] == closing_dict[char]:
+                    charStack.pop()
+                else:
+                    return False
+            else:
+                charStack.append(char)
+        return True if not charStack else False
