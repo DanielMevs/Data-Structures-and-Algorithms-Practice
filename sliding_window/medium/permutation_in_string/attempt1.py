1705476725af6fe0af0d57fa35da94e5800c1ef8
DanielMevs <dannymevs@hotmail.com>
2023-09-22 00:43:17 -0400

    started permutation string

---
+++ sliding_window/medium/permutation_in_string/attempt1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = {char: s1.count(char) for char in s1}
+        print(s1_count)
+        left_window, right_window = 0, len(s1)
+        while right_window <= len(s2):
+            sub_s2 = s2[left_window: right_window]
+            s2_count = {char: sub_s2.count(char) for char in sub_s2}
+            if s2_count == s1_count:
+                return True
+            else:
+                left_window, right_window = left_window + 1, right_window + 1
+            
+            
+        return False
+
+
+Solution().checkInclusion(s1='abbc', s2='xqcbabips')
+        
