905cdf690ada688e0a7ab7adbd82f443dbedd63b
DanielMevs <dannymevs@hotmail.com>
2023-11-18 12:23:37 -0500

    implemented trie prefix tree

---
+++ tries/medium/implement_trie_prefix_tree/solution1.py
@@ -0,0 +1,43 @@
+# - Trie Pronounced "try"
+
+class TrieNode:
+    
+    def __init__(self) -> None:
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return current.endOfWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+
+        current = self.root
+
+        for char in prefix:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+        return True
+        
