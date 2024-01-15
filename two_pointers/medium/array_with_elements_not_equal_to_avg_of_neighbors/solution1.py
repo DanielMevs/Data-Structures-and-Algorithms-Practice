e16d28fa54545222aab21b045724ef942707552a
DanielMevs <dannymevs@hotmail.com>
2024-01-15 10:43:11 -0500

    completed rearrange array such that its elements are not equal to the average of their neighbors

---
+++ two_pointers/medium/array_with_elements_not_equal_to_avg_of_neighbors/solution1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rearrangeArray(self, nums: list[int]) -> list[int]:
+        nums.sort()
+        result = []
+
+        left, right = 0, len(nums) - 1
+        while len(result) != len(nums):
+            result.append(nums[left])
+            left += 1
+
+            if left <= right:
+                result.append(nums[right])
+                right -= 1
+        
+        return result
+
+        
