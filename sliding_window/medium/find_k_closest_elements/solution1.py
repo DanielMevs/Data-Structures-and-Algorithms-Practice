542eb7eea126b2881a68a68e50a5bb4b137b130d
DanielMevs <dannymevs@hotmail.com>
2024-01-22 23:36:23 -0500

    completed find k closest elements

---
+++ sliding_window/medium/find_k_closest_elements/solution1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findClosestElements(self, arr: list[int], k: int, x: int) -> list[int]:
+        left, right = 0, len(arr) - k
+
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+                
+        return arr[left:left + k]
+        
