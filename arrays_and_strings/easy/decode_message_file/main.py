c83eb8b116a5ae7f03fb39678d828e34a24aee34
DanielMevs <dannymevs@hotmail.com>
2024-01-23 19:39:40 -0500

    completed decode message file

---
+++ arrays_and_strings/easy/decode_message_file/main.py
@@ -0,0 +1,39 @@
+def getElementsLeftOver(size, left):
+    return size - left
+    
+
+def decode(message_file: str) -> str:
+    lines = []
+    result = ''
+    left, right = 0, 1
+    count = 1
+    levels = []
+
+    with open(message_file, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+
+    sort_key = lambda x: int(x.split()[0])
+    lines.sort(key=sort_key)
+
+    
+    while right <= len(lines):
+        levels.append(lines[left: right])
+        left = right
+        count += 1
+        right += count 
+
+    remaining = getElementsLeftOver(len(lines), left)
+    if remaining:
+        levels.append(lines[left: left + remaining])
+    
+
+    for level in levels:
+        result += level[-1].split()[-1] + ' '
+
+
+    return result[:-1]
+
+
+if __name__ == "__main__":
+    print(decode('coding_qual_input.txt'))
