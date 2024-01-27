db3cf85a1e286ff1b24fed1eb6a28a956228b828
DanielMevs <dannymevs@hotmail.com>
2024-01-27 12:31:07 -0500

    completed asteroid collision

---
+++ stack/medium/asteroid_collision/solution1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def asteroidCollision(self, asteroids: list[int]) -> list[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            while stack and asteroid < 0 and stack[-1] > 0:
+                diff = asteroid + stack[-1]
+                if diff < 0:
+                    stack.pop()
+                elif diff > 0:
+                    asteroid = 0
+                else:
+                    asteroid = 0
+                    stack.pop()
+            if asteroid:
+                stack.append(asteroid)
+
+        return stack
