7c314b0e807c2210add6d373bb79e7d0f923c405
DanielMevs <dannymevs@hotmail.com>
2024-02-14 17:10:26 -0500

    completed minimize the maximum difference of pairs

---
+++ binary_search/medium/minimize_the_maximum_difference_of_pairs/solution1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        
+        def isValid(threshold):
+            i, count = 0, 0
+            while i < len(nums) - 1:
+                if abs(nums[i] - nums[i + 1]) <= threshold:
+                    count += 1
+                    i += 2
+                else:
+                    i += 1
+                if count == p:
+                    return True
+            return False
+        
+        if p == 0:
+            return 0
+        
+        nums.sort()
+        left, right = 0, 10**9
+        result = 10 ** 9
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isValid(mid):
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return result
+        
