6c2352a63f9addf48edf55bbe6efc440b6264ab7
DanielMevs <dannymevs@hotmail.com>
2024-03-15 12:32:47 -0400

    completed iterative and recursive binary tree inorder traversal

---
+++ trees/easy/binary_tree_inorder_traversal/recursive_solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> list[int]:
+        result = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            result.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+        return result
