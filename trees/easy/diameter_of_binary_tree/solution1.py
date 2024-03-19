a33b2f7f7ebb28af74e94cb0057e4dc1156eacf1
DanielMevs <dannymevs@hotmail.com>
2024-03-19 12:16:59 -0400

    completed balanced binary tree

--- trees/easy/diameter_of_binary_tree/solution1.py
+++ trees/easy/diameter_of_binary_tree/solution1.py
@@ -14,9 +14,9 @@ class Solution:
             if not root:
                 return -1
             left = dfs(root.left)
-            right = dfs(root.right)
+            right = dfs(root.right) 
 
-            result[0] = max(result[0], 2 + left + right)
+            result[0] = max(result[0], 2 + left + right) 
 
             return 1 + max(left, right)
 
