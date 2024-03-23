0621c23df6d55e000322969c82f5265f20c5baf8
DanielMevs <dannymevs@hotmail.com>
2024-03-23 10:13:31 -0400

    completed delete node in a bst

---
+++ trees/medium/delete_node_in_a_bst/solution1.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return root
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            # - Find the minimum from the right subtree
+            current = root.right
+            while current.left:
+                current = current.left
+            root.val = current.val
+            root.right = self.deleteNode(root.right, root.val)
+        
+        return root
