fc21756ae6c1129b89ccc898ce964bff4a93273e
DanielMevs <dannymevs@hotmail.com>
2024-04-23 21:01:07 -0400

    completed maximum subsequence score

---
+++ heaps_and_priority_queues/medium/maximum_subsequence_score/solution1.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+class Solution:
+    def maxScore(self, nums1: list[int], nums2: list[int], k: int) -> int:
+        pairs = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        pairs = sorted(pairs, key=lambda p: p[1], reverse=True)
+
+        minHeap = []
+        n1Sum = 0
+        result = 0
+
+        for n1, n2 in pairs:
+            n1Sum += n1
+            heapq.heappush(minHeap, n1)
+
+            if len(minHeap) > k:
+                n1Pop = heapq.heappop(minHeap)
+                n1Sum -= n1Pop
+            if len(minHeap) == k:
+                result = max(result, n1Sum * n2)
+        
+        return result
