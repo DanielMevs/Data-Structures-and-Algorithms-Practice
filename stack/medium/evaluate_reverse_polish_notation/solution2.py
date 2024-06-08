3c7f3887562c5e03cab84e47f8cac0a3aee601e2
DanielMevs <dannymevs@hotmail.com>
2024-06-08 13:12:39 -0400

    completed coin change

---
+++ stack/medium/evaluate_reverse_polish_notation/solution2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stackFuncs = {
+            '+': lambda x, y: x + y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: int(x/y),
+            '-': lambda x, y: x - y
+        }
+        stack = []
+
+        for token in tokens:
+            if token in stackFuncs:
+                op = stackFuncs[token]
+                num2 = stack.pop()
+                num1 = stack.pop()
+                
+                stack.append(op(num1, num2))
+            else:
+                stack.append(int(token))
+        
+        return stack.pop()
