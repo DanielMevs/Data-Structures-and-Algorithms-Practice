7b08bc0ad964dec010246724bdbd61ab08bcaef1
DanielMevs <dannymevs@hotmail.com>
2024-07-02 19:45:29 -0400

    completed gas station

---
+++ greedy/medium/gas_station/solution1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        result = 0
+        
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                result = i + 1
+        
+        return result
+
