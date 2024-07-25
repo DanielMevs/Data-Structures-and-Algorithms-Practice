4b872d9340f3c73ae3a9da94ed66655d68e44db8
DanielMevs <dannymevs@hotmail.com>
2024-07-25 19:09:47 -0400

    completed super pow

--- math_and_geometry/medium/super_pow/solution1.py
+++ math_and_geometry/medium/super_pow/solution1.py
@@ -1,6 +1,23 @@
 from typing import List
+MOD = 1337
 
 
 class Solution:
+    
     def superPow(self, a: int, b: List[int]) -> int:
-        pass
+        a %= MOD
+        result = 1
+        for digit in b:
+            result = (self.pow(result, 10) * self.pow(a, digit)) % MOD
+
+        return result
+    
+    def pow(self, a: int, power: int) -> int:
+        result = 1
+        while power:
+            if (power & 1):
+                result = (result * a) % MOD
+            a = (a * a) % MOD
+            power >>= 1
+        
+        return result
