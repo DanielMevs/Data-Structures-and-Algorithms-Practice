6eb5f0abb76d3969a6a5e72f2c4c4bd816deaa46
DanielMevs <dannymevs@hotmail.com>
2024-07-26 19:57:53 -0400

    completed multiply strings

---
+++ math_and_geometry/medium/multiply_strings/solution1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if '0' in [num1, num2]:
+            return '0'
+        
+        result = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                result[i1 + i2] += digit
+                result[i1 + i2 + 1] += result[i1 + i2] // 10  # add carry
+                result[i1 + i2] %= 10
+        
+        result, beginning = result[::-1], 0
+        # - To skip any leading zeros
+        while beginning < len(result) and result[beginning] == 0:
+            beginning += 1
+
+        result = map(str, result[beginning:])
+
+        return ''.join(result)
