d6464756d97f7dc3baf1ad320c9636043b8677ce
DanielMevs <dannymevs@hotmail.com>
2024-07-31 20:02:17 -0400

    completed counting bits

---
+++ bit_manipulation/easy/counting_bits/solution1.py
@@ -0,0 +1,15 @@
+# - Dynamic programming solution: O(n) time and space
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            result[i] = 1 + result[i - offset]
+        
+        return result
