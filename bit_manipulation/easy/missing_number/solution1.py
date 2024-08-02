02ff444baf5c1f5774c9bcbd6eb685bbf18edfdb
DanielMevs <dannymevs@hotmail.com>
2024-08-02 19:48:11 -0400

    completed mising number

---
+++ bit_manipulation/easy/missing_number/solution1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        result = len(nums)
+
+        for i in range(len(nums)):
+            result += (i - nums[i])
+
+        return result
