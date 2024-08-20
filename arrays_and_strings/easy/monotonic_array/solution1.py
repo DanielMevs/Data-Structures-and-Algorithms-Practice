b0972c16b8459c66585152e9f79a09db4ce2c37f
DanielMevs <dannymevs@hotmail.com>
2024-08-19 20:05:00 -0400

    completed monotonic array

---
+++ arrays_and_strings/easy/monotonic_array/solution1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if nums[-1] - nums[0] < 0:
+            nums.reverse()
+            
+        for i in range(len(nums) - 1):
+            if not nums[i] <= nums[i + 1]:
+                return False
+        
+        return True
