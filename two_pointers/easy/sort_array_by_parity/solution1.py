142662f3f003ec39d3ef35b60482a845e81050ad
DanielMevs <dannymevs@hotmail.com>
2024-09-25 19:38:21 -0400

    completed sort array by parity

---
+++ two_pointers/easy/sort_array_by_parity/solution1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] % 2 == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        return nums
