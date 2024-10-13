cb042999bcdcddae2b8940af505457169efbe63f
DanielMevs <dannymevs@hotmail.com>
2024-10-12 23:59:32 -0400

    minor refactor

--- stack/easy/make_the_string_great/solution1.py
+++ stack/easy/make_the_string_great/solution1.py
@@ -8,9 +8,6 @@ class Solution:
         def is_bad_pair(char1, char2):
             return abs(ord(char1) - ord(char2)) == ALT_CASE__DIFF
 
-        def is_bad_pair_v2(char1, char2):
-            return char1 != char2 and char1.lower() == char2.lower()
-
         for char in s:
             if stack and is_bad_pair(stack[-1], char):
                 stack.pop()
