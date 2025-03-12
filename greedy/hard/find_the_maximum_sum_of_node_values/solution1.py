ef266f051f3cdc60e847cd5e5c8b8ad9f5b539e3
DanielMevs <dannymevs@hotmail.com>
2025-03-11 23:06:30 -0400

    complete find the maximum sum of node values

---
+++ greedy/hard/find_the_maximum_sum_of_node_values/solution1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maximumValueSum(
+            self, nums: List[int],
+            k: int,
+            edges: List[List[int]]) -> int:
+        delta = [(n ^ k) - n for n in nums]
+        delta.sort(reverse=True)
+        result = sum(nums)
+
+        for i in range(0, len(nums), 2):
+            if i == len(nums) - 1:
+                break
+            path_delta = delta[i] + delta[i + 1]
+
+            if path_delta <= 0:
+                break
+
+            result += path_delta
+
+        return result
