e5790214c7a3302f4acd0e6b7bd65dd5dc614a4d
DanielMevs <dannymevs@hotmail.com>
2025-03-14 19:12:40 -0400

    complete minimum number of arrows to burst balloons

---
+++ intervals/medium/minimum_number_of_arrows_to_burst_balloons/solution1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+
+        result = len(points)
+        prev = points[0]
+
+        for i in range(1, len(points)):
+            curr = points[i]
+
+            if curr[0] <= prev[1]:
+                result -= 1
+                prev = [curr[0], min(prev[1], curr[1])]
+            else:
+                prev = curr
+
+        return result
