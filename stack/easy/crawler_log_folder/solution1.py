7ac76578d21aabbfbf121f25efcb93bd1824e476
DanielMevs <dannymevs@hotmail.com>
2025-09-09 11:38:59 -0400

    complete crawler log folder

---
+++ stack/easy/crawler_log_folder/solution1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        folder_count = 0
+        for folder in logs:
+            if folder == './':
+                continue
+            if folder == '../':
+                stack.pop() if stack else 0
+            else:
+                stack.append(folder)
+
+        while stack:  # count the directories left over
+            folder_count += 1
+            stack.pop()
+
+        return folder_count
